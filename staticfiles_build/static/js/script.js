e847704cc15ed83de6b2e29612e2de4cb7154a6e
Batas <nischal.batas88@gmail.com>
2023-02-07 14:38:07 +0545

    add

---
+++ staticfiles_build/static/js/script.js
@@ -0,0 +1,22 @@
+clock = () => {
+  let times = new Date();
+  dates = times.toLocaleDateString();
+  a = times.getHours() + ":" + times.getMinutes() + ":" + times.getSeconds();
+  document.getElementById("times").innerHTML = a + "  on  " + dates;
+  console.log(a);
+};
+
+setInterval(clock, 1000);
+
+clocks = () => {
+  let times = new Date();
+  times.setDate(times.getDate() + 7); // set date to 7 days in the future
+  a = times.getHours() + ":" + times.getMinutes() + ":" + times.getSeconds();
+  document.getElementById("time").innerHTML = a + "  on  " + times.toLocaleDateString();
+};
+
+setInterval(clocks, 1000);
+
+
+
+
